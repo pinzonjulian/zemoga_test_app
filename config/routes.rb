0e7f3c88bb63743de397e6e3d4d9a8131a3a725d
pinzonjulian <julianpinzoneslava@gmail.com>
2017-06-20 21:36:49 -0500

    add users controller and auth and non auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root to: 'users#show', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#home'
+
+  resources :users
 end
