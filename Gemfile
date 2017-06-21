9c6f7f15ba4d641ae6c768ea64da848f3dd86135
pinzonjulian <julianpinzoneslava@gmail.com>
2017-06-20 21:32:42 -0500

    add carrierwave

--- Gemfile
+++ Gemfile
@@ -18,13 +18,14 @@ gem "autoprefixer-rails"
 gem 'jquery'
 gem 'foundation-rails'
 
+gem 'carrierwave', '~> 1.0'
+
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
 gem 'devise'
-
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  mount_uploader :avatar, AvatarUploader
 
   def profile_info_is_complete?
     name.present? && last_name.present? && description.present? && twitter_handle.present?
