1a373daa4384c80dbafa2c8b1bc6d37826742456
pinzonjulian <julianpinzoneslava@gmail.com>
2017-06-20 21:00:23 -0500

    add fields to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def profile_info_is_complete?
+    name.present? && last_name.present? && description.present? && twitter_handle.present?
+  end
 end
