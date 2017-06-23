1e32c443eea79a19f3f57a013367b11bccb5d83b
pinzonjulian <julianpinzoneslava@gmail.com>
2017-06-22 20:18:34 -0500

    nothing special

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -1,6 +1,6 @@
 module Accessible
   extend ActiveSupport::Concern
-  
+
   included do
     before_action :check_user
   end
@@ -10,10 +10,8 @@ module Accessible
   def check_user
     flash.clear
     if current_admin
-      puts '**************************** inside current_admin'
       redirect_to(rails_admin.dashboard_path) && return
     elsif current_user
-      puts '**************************** inside current_user'
       redirect_to(authenticated_user_root_path) && return
     end
   end
