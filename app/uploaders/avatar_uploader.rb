3868c4be2476f85ae29cdffc31f0aa89d75c6ec2
pinzonjulian <julianpinzoneslava@gmail.com>
2017-06-26 17:35:47 -0500

    wip

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -11,7 +11,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "publis/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   process resize_to_fit: [800, 800]
