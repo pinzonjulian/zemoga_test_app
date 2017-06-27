717ad73229bf2e6d8b496c2997874cc161bf44a4
pinzonjulian <julianpinzoneslava@gmail.com>
2017-06-27 00:38:42 -0500

    fix region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV['S3_SECRET_KEY'],                        # required
       aws_secret_access_key: ENV['S3_ACCESS_KEY'],                        # required
-      region:                'eu-east-1',                  # optional, defaults to 'us-east-1'
+      region:                'us-east-1',                  # optional, defaults to 'us-east-1'
     }
     config.fog_directory  = ENV['S3_BUCKET']                          # required
     config.fog_public     = false                                        # optional, defaults to true
