633503e0bbb2d73324ed6d25f7811b2a09af8d09
pinzonjulian <julianpinzoneslava@gmail.com>
2017-06-27 20:42:34 -0500

    deletes host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,7 @@ if Rails.env.production?
       provider: 'AWS',
       aws_access_key_id: ENV['S3_SECRET_KEY'],
       aws_secret_access_key: ENV['S3_ACCESS_KEY'],
-      region: 'us-east-1',
-      host: 'https://s3.zemoga-test-app.herokuapp.com:8080'
+      region: 'us-east-1'
     }
     config.fog_directory  = ENV['S3_BUCKET']
   end
