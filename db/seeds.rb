aa3be9e8768dfcaad706af154f4038b3cc1850b5
pinzonjulian <julianpinzoneslava@gmail.com>
2017-07-07 13:38:53 -0500

    add commented methods to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(email: 'admin@example.com', password: 'mxl293AidbfZow92hdii')
+
+
+# Admin.create!(email: 'admin@example.com', password: 'mxl293AidbfZow92hdii')
+# User.create!(name: '', last_name: 'link', description: 'asdfasdfsa', twitter_handle: 'hanlde', remote_avatar_url: 'https://api.adorable.io/avatars/285/userNUM.png', email: 'test@test.com', password: 'mypassword')
